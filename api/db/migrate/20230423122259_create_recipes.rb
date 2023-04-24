bd354a8cfa0c39cdc756e2423c81aa97635d5864
Jesalim <jessyculture@gmail.com>
2023-04-24 11:18:58 +0300

    added seed data

--- api/db/migrate/20230423122259_create_recipes.rb
+++ api/db/migrate/20230423122259_create_recipes.rb
@@ -1,12 +1,13 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.belongs_to :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :name
-      t.integer :rating
-      t.string :ingredients
       t.string :country
+      t.integer :rating
       t.integer :people_served
+      t.string :ingredients
+      t.string :image_url
 
       t.timestamps
     end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -7,10 +7,9 @@
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
-#
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_082913) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_23_122259) do
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
@@ -20,10 +19,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_19_082913) do
   end
 
   create_table "recipe_ingredients", force: :cascade do |t|
-    t.integer "recipe_id"
-    t.integer "ingredient_id"
+    t.integer "recipe_id", null: false
+    t.integer "ingredient_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -47,5 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_19_082913) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "recipe_ingredients", "ingredients"
+  add_foreign_key "recipe_ingredients", "recipes"
   add_foreign_key "recipes", "users"
 end
