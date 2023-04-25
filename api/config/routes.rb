3a8ee18079866fdd9b417da109c6bce487530658
Abdub2 <abdubdoyo2005@gmail.com>
2023-04-25 12:05:21 +0300

    Simplified routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -6,22 +6,23 @@ Rails.application.routes.draw do
   #resources :recipes, only: [:show, :create, :destroy, :update]
 
    # user
-   post '/register', to: 'users#create'
-   put '/users/:id', to: 'users#update'
-   delete '/users/:id', to: 'users#destroy'
+   resources :users, only: [:index, :destroy, :update]
+
+   post '/signup', to: 'users#create'
    get '/users/:id', to: 'users#show'
- 
- 
+
+
    # sessions
    post '/login', to: 'sessions#create'
    delete '/logout', to: 'sessions#destroy'
    get '/me', to: 'sessions#user_session'
 
     # recipes
-  post '/recipes', to: 'recipes#create'
-  patch '/recipes/:id', to: 'recipes#update'
-  delete '/recipes/:id', to: 'recipes#destroy'
-  get '/recipes', to: 'recipes#index'
-  get '/recipes/:id', to: 'recipes#show'
+  resources :recipes, only: [:index, :show, :create, :update, :destroy]
+  # post '/recipes', to: 'recipes#create'
+  # patch '/recipes/:id', to: 'recipes#update'
+  # delete '/recipes/:id', to: 'recipes#destroy'
+  # get '/recipes', to: 'recipes#index'
+  # get '/recipes/:id', to: 'recipes#show'
   get "/search", to: "recipes#search"
 end
