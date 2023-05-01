bb30c0af83c016019018ab6ec19492ef6e07c4bc
Jesalim <jessyculture@gmail.com>
2023-05-01 22:40:36 +0300

    config err

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-dom/client'],
-    },
-  },
+    outDir: 'build'
+  }
 });
